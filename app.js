5b8f8326d34a0efb0ac2df27e06fa4c2b57e950b
mykhailo.petrenko <mihal@seven-bytes.com>
2017-09-29 18:35:15 +0300

    Watch and restart service by `nodemon`

--- app.js
+++ app.js
@@ -2,13 +2,13 @@ const express = require('express');
 
 const app = express();
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 80;
 
 app.get('/', function(req, res) {
-    res.write("HELLO");
+    res.write("Hello World!");
     res.end();
 });
 
 app.listen(PORT, function() {
-    console.log('Server started');
+    console.log(`Server started. PORT: ${PORT}`);
 });
